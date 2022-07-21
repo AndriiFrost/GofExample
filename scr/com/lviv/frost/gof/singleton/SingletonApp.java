8939747cf1fc0acdeaf1e1a9cfbf88a189beb1ae
Andrii Moroz <Andrii_Moroz@epam.com>
2022-07-21 22:57:13 +0300

    Singleton

---
+++ scr/com/lviv/frost/gof/singleton/SingletonApp.java
@@ -0,0 +1,13 @@
+package com.lviv.frost.gof.singleton;
+
+public class SingletonApp {
+    public static void main(String[] args) {
+        ProgramLogger instance = ProgramLogger.getInstance();
+        instance.addLog();
+        System.out.println(instance.getLogCount());
+        System.out.println("=================");
+        System.out.println(instance);
+        System.out.println(ProgramLogger.getInstance().toString());
+        System.out.println(ProgramLogger.getInstance().toString());
+    }
+}
